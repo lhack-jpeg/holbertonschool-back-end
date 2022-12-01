1f259793fdc5cd3a8aa606ab67d805bb1b07689b
Luke Hackett <luke-hackett@outlook.com>
2022-12-01 13:52:21 +1100

    revert back to original method

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,22 +6,19 @@ https://jsonplaceholder.typicode.com/
 
 
 if __name__ == '__main__':
-    import json
+    import requests
     from sys import argv
-    import urllib.request
 
     user_id = argv[1]
     user_info_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(
         user_id
     )
-    user_req = urllib.request.urlopen(user_info_url)
-    user_data = json.loads(user_req.read())
+    user_data = requests.get(user_info_url).json()
     emp_name = user_data.get('name')
 
-    todo_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
+    todo_url_req = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
         user_id)
-    todo_req = urllib.request.urlopen(todo_url)
-    todo_data = json.loads(todo_req.read())
+    todo_data = requests.get(todo_url_req).json()
     todo_count = len(todo_data)
     todo_completed = [d for d in todo_data if d.get('completed')]
     len_completed = len(todo_completed)
