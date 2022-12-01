b1730d781f6e879052ff4d340180487d94030369
Luke Hackett <luke-hackett@outlook.com>
2022-12-01 15:00:01 +1100

    change from urllib to requests

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -7,23 +7,20 @@ https://jsonplaceholder.typicode.com/
 
 if __name__ == '__main__':
     import csv
-    import json
+    import requests
     from sys import argv
-    import urllib.request
 
     user_id = argv[1]
     user_info_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(
         user_id
     )
-    user_req = urllib.request.urlopen(user_info_url)
-    user_data = json.loads(user_req.read())
+    user_data = requests.get(user_info_url).json()
     emp_name = user_data.get('username')
     file_name = user_id + '.csv'
 
     todo_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
         user_id)
-    todo_req = urllib.request.urlopen(todo_url)
-    todo_data = json.loads(todo_req.read())
+    todo_data = requests.get(todo_url).json()
 
     with open(file_name, 'w', encoding='utf-8') as f:
         writer = csv.writer(f, delimiter=',', quotechar='"',
