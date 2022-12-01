ff8cbf438ded47ad001b5f898ec2375ccb4f2e73
Luke Hackett <luke-hackett@outlook.com>
2022-12-01 12:26:41 +1100

    fix line length

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,9 +6,9 @@ https://jsonplaceholder.typicode.com/
 
 
 if __name__ == '__main__':
+    import json
     import requests
     from sys import argv
-    import json
 
     user_id = argv[1]
     user_info_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(
@@ -18,9 +18,9 @@ if __name__ == '__main__':
     user_data = json.loads(user_req.text)
     emp_name = user_data.get('name')
 
-    todo_url_req = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
+    todo_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
         user_id)
-    todo_req = requests.get(todo_url_req)
+    todo_req = requests.get(todo_url)
     todo_data = json.loads(todo_req.text)
     todo_count = len(todo_data)
     todo_completed = len([d for d in todo_data if d.get('completed') == True])
