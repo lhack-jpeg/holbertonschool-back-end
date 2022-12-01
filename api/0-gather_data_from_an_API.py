c8d4eb4e51bd5bb2002dff514c0ddf9634f48a1e
Luke Hackett <luke-hackett@outlook.com>
2022-12-01 14:05:22 +1100

    add comments

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -10,16 +10,19 @@ if __name__ == '__main__':
     from sys import argv
 
     user_id = argv[1]
+    # Get the employee name
     user_info_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(
         user_id
     )
     user_data = requests.get(user_info_url).json()
     emp_name = user_data.get('name')
 
+    # Get tasks related to the userid
     todo_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
         user_id)
     todo_data = requests.get(todo_url).json()
     todo_count = len(todo_data)
+    # Create a list of only tasks that are completed
     todo_completed = [d for d in todo_data if d.get('completed')]
     len_completed = len(todo_completed)
     first_line = 'Employee {} is done with tasks({}/{}):'.format(
