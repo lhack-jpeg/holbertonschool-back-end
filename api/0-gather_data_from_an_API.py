9fe6295cf940b8d9a757d919cb9bd8da62c67802
Luke Hackett <luke-hackett@outlook.com>
2022-12-01 12:32:17 +1100

    moved len to new variable

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -16,19 +16,20 @@ if __name__ == '__main__':
     )
     user_req = requests.get(user_info_url)
     user_data = json.loads(user_req.text)
-    emp_name = user_data.get('name')
+    emp_name = user_data.get('name').strip()
 
     todo_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
         user_id)
     todo_req = requests.get(todo_url)
     todo_data = json.loads(todo_req.text)
     todo_count = len(todo_data)
-    todo_completed = len([d for d in todo_data if d.get('completed')])
+    todo_completed = [d for d in todo_data if d.get('completed')]
+    len_completed = len(todo_completed)
     first_line = 'Employee {} is done with tasks({}/{}):'.format(
         emp_name,
-        todo_completed,
+        len_completed,
         todo_count
     )
     print(first_line)
-    for task in todo_data:
+    for task in todo_completed:
         print('\t {}'.format(task.get('title')))
