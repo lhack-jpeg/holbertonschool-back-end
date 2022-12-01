a2c6c0c538d940fb5018cbf0b1206eb3744b35cf
Luke Hackett <luke-hackett@outlook.com>
2022-12-01 12:18:23 +1100

    print out user/tasks from api

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''
+This module gathers data from a mockup api.
+https://jsonplaceholder.typicode.com/
+'''
+
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    import json
+
+    user_id = argv[1]
+    user_info_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(
+        user_id
+    )
+    user_req = requests.get(user_info_url)
+    user_data = json.loads(user_req.text)
+    emp_name = user_data.get('name')
+
+    todo_url_req = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
+        user_id)
+    todo_req = requests.get(todo_url_req)
+    todo_data = json.loads(todo_req.text)
+    todo_count = len(todo_data)
+    todo_completed = len([d for d in todo_data if d.get('completed') == True])
+    first_line = 'Employee {} is done with tasks({}/{}):'.format(
+        emp_name,
+        todo_completed,
+        todo_count
+    )
+    print(first_line)
+    for task in todo_data:
+        print('\t {}'.format(task.get('title')))
