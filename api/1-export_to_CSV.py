c729706d28584a0d61e51d96b58a567de7e6aa7c
Luke Hackett <luke-hackett@outlook.com>
2022-12-01 14:05:03 +1100

    change writing method for api

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -25,17 +25,14 @@ if __name__ == '__main__':
     todo_req = urllib.request.urlopen(todo_url)
     todo_data = json.loads(todo_req.read())
     list_of_todos = []
-    for todo in todo_data:
-        todo_dict = {}
-        todo_dict.update({'USER_ID': user_id})
-        todo_dict.update({'USERNAME': emp_name})
-        todo_dict.update({'TASK_COMPLETED_STATUS': todo.get('completed')})
-        todo_dict.update({'TASK_TITLE': todo.get('title')})
-        list_of_todos.append(todo_dict)
 
     fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
 
     with open(file_name, 'w', encoding='utf-8') as f:
-        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer = csv.writer(f, delimiter=',', quotechar='"',
+                            quoting=csv.QUOTE_ALL)
 
-        writer.writerows(list_of_todos)
+        for todo in todo_data:
+            todo_list = [user_id, emp_name,
+                         todo.get('completed'), todo.get('title')]
+            writer.writerow(todo_list)
